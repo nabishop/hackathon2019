acb555c6d0369b0afe5fc444802b46f8519c23d1
nabishop <35745821+nabishop@users.noreply.github.com>
2019-02-02 11:54:35 -0800

    Update PlaylistPersistence.cs

--- RhopikApi/App_Code/PlaylistPersistence.cs
+++ RhopikApi/App_Code/PlaylistPersistence.cs
@@ -1,8 +1,15 @@
-﻿
+﻿using MySql.Data;
+
 namespace RhopikApi.App_Code
 {
     public class PlaylistPersistence
     {
+        private MySql.Data.MySqlClient.MySqlConnection connection;
+
+        public PlaylistPersistence()
+        {
+            string myConnectionString = "slohacks2019.mysql.database.azure.com";
+        }
 
     }
 }
