49c95feffaef6a7e3e3063b02c0b954ccc1ff9c6
Sarah <sarahbae1998@gmail.com>
2019-02-02 10:18:28 -0800

    hh

--- src/app/playlist.ts
+++
@@ -1,7 +0,0 @@
-import { Song } from './song';
-export class Playlist {
-  id: number;
-  name: string;
-  vibe: string;
-  songs: Song[]
-}
--- src/app/safe-post.interface.ts
+++
@@ -1,6 +0,0 @@
-import { SafeHtml } from '@angular/platform-browser';
-
-export interface SafePost {
-    'title': SafeHtml;
-    'content': SafeHtml;
-}
