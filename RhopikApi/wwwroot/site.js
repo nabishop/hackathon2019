73eedf48befd2b6512fc1ddca0c7889aa7fcf46d
Brian Tsai <34466349+Tsai16@users.noreply.github.com>
2019-02-02 14:08:52 -0800

    Add files via upload

---
+++ RhopikApi/wwwroot/site.js
@@ -0,0 +1,128 @@
+﻿const uri = "api/song";
+let names = null;
+function getCount(data) {
+    const el = $("#counter");
+    let name = "song";
+    if (data) {
+        if (data > 1) {
+            name = "songs";
+        }
+        el.text(data + " " + name);
+    } else {
+        el.text("No " + name);
+    }
+}
+
+$(document).ready(function () {
+    getData();
+});
+
+function getData() {
+    $.ajax({
+        type: "GET",
+        url: uri,
+        cache: false,
+        success: function (data) {
+            const tBody = $("#songs");
+
+            $(tBody).empty();
+
+            getCount(data.length);
+
+            $.each(data, function (key, item) {
+                const tr = $("<tr></tr>")
+                    .append($("<td></td>").text(item.name))
+                    .append($("<td></td>").text(item.artist))
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Edit</button>").on("click", function () {
+                                editItem(item.id);
+                            })
+                        )
+                    )
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Delete</button>").on("click", function () {
+                                deleteItem(item.id);
+                            })
+                        )
+                    );
+
+                tr.appendTo(tBody);
+            });
+
+            songs = data;
+        }
+    });
+}
+
+function addSong() {
+    const item = {
+        name: $("#add-name").val(),
+        artist: $("#add-artist").val()
+    };
+
+    $.ajax({
+        type: "POST",
+        accepts: "application/json",
+        url: uri,
+        contentType: "application/json",
+        data: JSON.stringify(item),
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert("Something went wrong!");
+        },
+        success: function (result) {
+            getData();
+            $("#add-name").val("");
+            $("#add-artist").val("");
+        }
+    });
+}
+
+function deleteItem(id) {
+    $.ajax({
+        url: uri + "/" + id,
+        type: "DELETE",
+        success: function (result) {
+            getData();
+        }
+    });
+}
+
+function editItem(id) {
+    $.each(songs, function (key, item) {
+        if (item.id === id) {
+            $("#edit-name").val(item.name);
+            $("#edit-artist").val(item.artist);
+            $("#edit-id").val(item.id);
+            
+        }
+    });
+    $("#spoiler").css({ display: "block" });
+}
+
+$(".my-form").on("submit", function () {
+    const item = {
+        name: $("#edit-name").val(),
+        artist: $("#edit-artist").val(),
+        id: $("#edit-id").val()
+    };
+
+    $.ajax({
+        url: uri + "/" + $("#edit-id").val(),
+        type: "PUT",
+        accepts: "application/json",
+        contentType: "application/json",
+        data: JSON.stringify(item),
+        success: function (result) {
+            getData();
+        }
+    });
+
+    closeInput();
+    return false;
+});
+
+function closeInput() {
+    $("#spoiler").css({ display: "none" });
+}
