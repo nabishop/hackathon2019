920ec90dfed7a5bd9e730b409f4fd898c69ea472
nabishop <35745821+nabishop@users.noreply.github.com>
2019-02-03 01:11:43 -0800

    REST API BITCH

--- RhopikApi/RhopikApi/Controllers/UserController.cs
+++ RhopikApi/RhopikApi/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 using RhopikApi.Models;
+using System;
 using System.Collections.Generic;
 
 namespace RhopikApi.Controllers
@@ -26,20 +28,38 @@ namespace RhopikApi.Controllers
 
         // POST: api/User
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post(UserItem userItem)
         {
+            using (MySqlConnection conn = new MySqlConnection(_context.ConnectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO student VALUES(" + userItem.Name + ", " + userItem.Name, conn);
+                cmd.ExecuteNonQuery();
+            }
         }
 
         // PUT: api/User/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(UserItem userItem)
         {
+            using (MySqlConnection conn = new MySqlConnection(_context.ConnectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("UPDATE users SET password=" + userItem.Password + " WHERE user_id=" + userItem.Id, conn);
+                cmd.ExecuteNonQuery();
+            }
         }
 
-        // DELETE: api/ApiWithActions/5
+        // DELETE: api/
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            using (MySqlConnection conn = new MySqlConnection(_context.ConnectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("DELETE FROM users where user_id=" + id, conn);
+                cmd.ExecuteNonQuery();
+            }
         }
 
         public UserController(UserItemContext context)
