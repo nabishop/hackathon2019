3d25504947ee3906d1f8e37778fa71ec0cac80b2
Brian Tsai <34466349+Tsai16@users.noreply.github.com>
2019-02-02 16:35:37 -0800

    Add files via upload

---
+++ RhopikApi/RhopikApi/Models/SongItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RhopikApi.Models
+{
+   
+    public class SongItem
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public long song_id { get; set; }
+        public string name { get; set; }
+        public string artist { get; set; }
+        public string genre { get; set; }
+        public string length { get; set; }
+        public string album_covers { get; set; }
+    }
+}
+
---
+++ RhopikApi/RhopikApi/Startup.cs
@@ -0,0 +1,53 @@
+﻿// Unused usings removed
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols;
+using RhopikApi.Models;
+
+namespace RhopikApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the 
+        //container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = "Server=slohacks2019.mysql.database.azure.com;Database=slohacks;User=tschoppcity;Password=password_1;Trusted_Connection=True;";
+            services.AddDbContext<SongItemContext>(opt =>
+                opt.(connectionString));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP 
+        //request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for 
+                // production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
